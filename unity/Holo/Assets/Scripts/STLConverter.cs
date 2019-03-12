d9fcf43b5ffd462326e92ee07ff99a8e35488752
Bartlomiej Kuchnowski <bartlomiej.kuchnowski@microscopeit.com>
2019-03-12 11:04:06 +0100

    :construction: made import into a separate class

--- unity/Holo/Assets/Scripts/STLConverter.cs
+++ unity/Holo/Assets/Scripts/STLConverter.cs
@@ -6,50 +6,60 @@ using System.Collections.Generic;
 using System.IO;
 
 
-public class STLConverter
+public class STLSeriesConverter
 {
+    GameObject series_GameObject;
+
+
     [MenuItem("Holo/ImportSTL")]
     public static void ConvertSTL()
     {
-        STLConverter stlconverter = new STLConverter();
+        STLSeriesConverter stlSeriesConverter = new STLSeriesConverter();
+        STLImporter stlImporter = new STLImporter();
 
-        GameObject imported_Mesh = stlconverter.Import_stl();
-        imported_Mesh.AddComponent<BlendShapeAnimation>();
-        stlconverter.ExportToPrefab(imported_Mesh);
+        stlSeriesConverter.series_GameObject = stlImporter.Get_GameObject();
+        stlSeriesConverter.series_GameObject.AddComponent<BlendShapeAnimation>();
+
+        stlSeriesConverter.ExportToPrefab();
     }
-
-    //Imports STL series and turns it into a game object
-    private GameObject Import_stl()
-    {
-        string[] file_paths = this.Get_file_paths();
-        GameObject imported_Mesh = this.load_files(file_paths);
-
-        return imported_Mesh;
-    }
-
-    // gets path to the subsequent STL meshes stored in a root folder.
-    private string[] Get_file_paths()
-    {
-        string[] path_array = { 'Foo' };
-        // TODO: Getting the actual path
-        return path_array
-    }
-
-
-    //loads separate files into one GameObject
-    private GameObject load_files(string[] file_paths)
-    {
-
-
-
-        return
-    }
-
 
     // Exports finished GameObject to a .prefab
-    private void ExportToPrefab(GameObject Mesh)
+    private void ExportToPrefab()
     {
         //TODO:  Exporting imported meshes to a prefab.
     }
 
+}
+
+public class STLImporter
+{
+    GameObject imported_STLs = new GameObject();
+    List<string> file_paths = new List<string>();
+
+    public STLImporter()
+    {
+        this.Get_file_paths();
+    }
+
+
+    // gets path to the subsequent STL meshes stored in a root folder.
+    private void Get_file_paths()
+    {
+        string root_folder = EditorUtility.OpenFolderPanel("Select STL series root folder", "", "");
+        string[] file
+    }
+
+
+    //loads separate files into one GameObject
+    private void Load_files(string[] file_paths)
+    {
+        
+        bool first_mesh = true;
+
+    }
+
+    public GameObject Get_GameObject()
+    {
+        return this.imported_STLs;
+    }
 }
