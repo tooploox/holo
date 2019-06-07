8224bb651d6ef99b9148f73cda477a765b05d14b
Bartlomiej <bartlomiej.kuchnowski@microscopeit.com>
2019-06-07 09:11:32 +0200

    HOL-40 Added Asset Bundle Loader to the Editor

---
+++ unity/Holo/Assets/Editor/AssetBundleEditorLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+
+public class AssetBundleEditorLoader
+{
+    [MenuItem("Holo/Load model from a AssetBundle's GameObject")]
+    public static void LoadAssetBundle()
+    {
+        AssetBundleLoader assetBundleLoader = new AssetBundleLoader();
+        string bundlePath = EditorUtility.OpenFilePanel("Get The Bundle","","");
+        assetBundleLoader.LoadBundle(bundlePath);
+        assetBundleLoader.LoadMainGameObject();
+        assetBundleLoader.InstantiateMainGameObject();
+    }
+}
