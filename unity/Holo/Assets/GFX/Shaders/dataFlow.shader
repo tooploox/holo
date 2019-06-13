baab17e164c07b52a38f1ae1eaf3ac4884c5e616
Barnaba Mikulowski <vaticinator@magor.pl>
2019-06-13 14:04:57 +0200

    dataflow based on the points topology mesh

---
+++ unity/Holo/Assets/GFX/Shaders/dataFlow.shader
@@ -0,0 +1,85 @@
+﻿Shader "Holo/dataflow"
+{
+	Properties
+	{
+		_MainTex("Ground (RGB)", 2D) = "white" {}
+		_WeightFactor("Line weight", Range(.01,.1)) = 0.01
+		_LengthFactor("Line weight", Range(.01,.1)) = 0.01
+	}
+		SubShader
+	{
+		Tags { "RenderType" = "Opaque" }
+		Cull Off
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma geometry geom
+			#include "UnityCG.cginc"
+
+			struct v2g
+			{
+				float4 vertex : POSITION;
+				float3 normal : NORMAL;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct g2f
+			{
+				float4 pos : SV_POSITION;
+				float2 uv : TEXCOORD0;
+				float4 col : COLOR;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			float _WeightFactor;
+			float _LengthFactor;
+
+			v2g vert(appdata_base v)
+			{
+				v2g o;
+				o.vertex = v.vertex;
+				o.uv = TRANSFORM_TEX(v.texcoord, _MainTex);
+				o.normal = v.normal;
+				return o;
+			}
+
+			[maxvertexcount(24)]
+			void geom(triangle v2g IN[3], inout TriangleStream<g2f> tristream)
+			{
+				g2f o;
+				float3 normalFace = normalize(cross(normalize(IN[0].normal), _WorldSpaceCameraPos));
+				
+				for (int i = 0; i < 1; i++)
+				{
+					o.pos = UnityObjectToClipPos(IN[i].vertex);
+					o.uv = IN[i].uv;
+					o.col = fixed4(0., 0., 0., 1.);
+					tristream.Append(o);
+
+					o.pos = UnityObjectToClipPos(IN[i].vertex + float4(normalFace, 0) * _WeightFactor);
+					o.uv = IN[i].uv;
+					o.col = fixed4(0., 0., 0., 1.);
+					tristream.Append(o);
+
+					o.pos = UnityObjectToClipPos(IN[i].vertex + float4(normalize(IN[i].normal) * _LengthFactor, 1));
+					o.uv = IN[i].uv;
+					o.col = fixed4(1., 1., 1., 1.);
+					tristream.Append(o);
+
+					tristream.RestartStrip();
+				}
+			}
+
+			fixed4 frag(g2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.uv) * i.col;
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
--- unity/Holo/Assets/Scripts/model_with_plate/DataFlowVizualizer.cs
+++ unity/Holo/Assets/Scripts/model_with_plate/DataFlowVizualizer.cs
@@ -23,7 +23,8 @@ public class DataFlowVizualizer : MonoBehaviour
             points = new List<Vector3>[frameCount];
             vectors = new List<Vector3>[frameCount];
             endPoints = new List<Vector3>[frameCount];
-            for (int i=0; i<frameCount; i++) {
+            for (int i = 0; i < frameCount; i++)
+            {
                 points[i] = new List<Vector3>();
                 vectors[i] = new List<Vector3>();
                 endPoints[i] = new List<Vector3>();
@@ -38,11 +39,14 @@ public class DataFlowVizualizer : MonoBehaviour
 
     private void CreateMesh()
     {
-        int linesCount = points[0].Count;
-        Vector3[] vertices = new Vector3[linesCount * 4];
-        for (int i = 0; i<vertices.Length; i++)
+        int pointsCount = points[0].Count;
+        Vector3[] vertices = new Vector3[pointsCount];
+        Vector3[] vectorsAsNormals = new Vector3[pointsCount];
+        for (int i = 0; i < vertices.Length; i++)
+        {
             vertices[i] = Vector3.zero;
-
+            vectorsAsNormals[i] = Vector3.zero;
+        }
         //if base mesh should looks like first blendShape, now vertices in base mesh are 0,0,0:
         //Array.Copy(points[0].ToArray(), vertices, points[0].Count);
         //Array.Copy(endPoints[0].ToArray(), 0, vertices, points[0].Count, endPoints[0].Count);
@@ -53,17 +57,8 @@ public class DataFlowVizualizer : MonoBehaviour
 
         for (int frame = 0; frame < frameCount; frame++)
         {
-            vertices = new Vector3[linesCount * 4];
-            Array.Copy(points[frame].ToArray(), vertices, linesCount);
-            Array.Copy(endPoints[frame].ToArray(), 0, vertices, linesCount * 1, linesCount);
-            Array.Copy(points[frame].ToArray(), 0, vertices, linesCount * 2, linesCount);
-            Array.Copy(endPoints[frame].ToArray(), 0, vertices, linesCount * 3, linesCount);
-            //put vector data inside normals data
-            Vector3[] vectorsAsNormals = new Vector3[linesCount * 4];
-            Array.Copy(vectors[frame].ToArray(), vectorsAsNormals, linesCount);
-            Array.Copy(vectors[frame].ToArray(), 0, vectorsAsNormals, linesCount * 1, linesCount);
-            Array.Copy(vectors[frame].ToArray(), 0, vectorsAsNormals, linesCount * 2, linesCount);
-            Array.Copy(vectors[frame].ToArray(), 0, vectorsAsNormals, linesCount * 3, linesCount);
+            vertices = points[frame].ToArray();
+            vectorsAsNormals = vectors[frame].ToArray(); //put vector data inside normals data
 
             //add blendShape
             mesh.AddBlendShapeFrame(frame.ToString(), 100f, vertices, vectorsAsNormals, null); //last null is a tangentNormal, we can put there alpha and beta data
@@ -74,19 +69,13 @@ public class DataFlowVizualizer : MonoBehaviour
             Debug.LogWarning("points[0].Count != vectors[0].Count " + points[0].Count + " " + endPoints[0].Count);
         }
 
-        Vector2[] uvs = new Vector2[vertices.Length];
-        int[] indices = new int[linesCount * 4];
+        Vector2[] uvs = new Vector2[pointsCount];
+        int[] indices = new int[pointsCount];
 
-        for (int i = 0; i < indices.Length / 4; i++)
+        for (int i = 0; i < pointsCount; i++)
         {
-            indices[4 * i] =     (linesCount * 0) + i; //start point
-            indices[4 * i + 1] = (linesCount * 2) + i; //end point
-            indices[4 * i + 2] = (linesCount * 1) + i; //start point
-            indices[4 * i + 3] = (linesCount * 3) + i; //end point
-            uvs[0 * linesCount + i] = new Vector2(0, 0);
-            uvs[1 * linesCount + i] = new Vector2(1, 0);
-            uvs[2 * linesCount + i] = new Vector2(1, 1);
-            uvs[3 * linesCount + i] = new Vector2(0, 1);            
+            indices[i] = i;
+            uvs[i] = new Vector2(0, 0);
         }
 
         mesh.uv = uvs;
@@ -98,15 +87,15 @@ public class DataFlowVizualizer : MonoBehaviour
             // - Lines: Each two indices in the mesh index buffer form a line.
             // - LineStrip:  First two indices form a line, and then each new index connects a new vertex to the existing line strip.
             // - Points: In most of use cases, mesh index buffer should be "identity": 0, 1, 2, 3, 4, 5, ...
-            MeshTopology.Quads,
+            MeshTopology.Points,
             // submesh
             0);
 
         // save Mesh to file
-        AssetDatabase.DeleteAsset("Assets/test_lines.mesh");
-        AssetDatabase.CreateAsset(mesh, "Assets/test_lines.mesh");
+        AssetDatabase.DeleteAsset("Assets/test_points.mesh");
+        AssetDatabase.CreateAsset(mesh, "Assets/test_points.mesh");
 
-        GameObject go = new GameObject("Test Mesh SetTopology");
+        GameObject go = new GameObject("Test Points Topology");
         MeshFilter meshFilter = go.AddComponent<MeshFilter>();
         meshFilter.mesh = mesh;
         SkinnedMeshRenderer meshRenderer = go.AddComponent<SkinnedMeshRenderer>();
@@ -115,8 +104,8 @@ public class DataFlowVizualizer : MonoBehaviour
         go.AddComponent<BlendShapeAnimation>();
 
         // save GameObject to file
-        AssetDatabase.DeleteAsset("Assets/test_lines_game_object.prefab");
-        PrefabUtility.SaveAsPrefabAsset(go, "Assets/test_lines_game_object.prefab");
+        AssetDatabase.DeleteAsset("Assets/test_points_game_object.prefab");
+        PrefabUtility.SaveAsPrefabAsset(go, "Assets/test_points_game_object.prefab");
     }
 
     void ReadVTKData(int frame, string fileVtk)
@@ -138,7 +127,7 @@ public class DataFlowVizualizer : MonoBehaviour
             */
 
             string[] split = l.Trim().Split(" ".ToCharArray());
-            
+
             double x, y, z;
             if (split.Length == 3
                 && double.TryParse(split[0], NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out x)
@@ -165,5 +154,5 @@ public class DataFlowVizualizer : MonoBehaviour
         else
             Debug.Log(fileVtk + ": \r\n " + points[frame].Count + " points in Data Flow");
     }
-    
+
 }
