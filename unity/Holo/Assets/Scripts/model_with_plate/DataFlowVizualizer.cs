0163ee0eb8f17ca571f7469e05941611ef65659e
Barnaba Mikulowski <vaticinator@magor.pl>
2019-06-19 17:24:13 +0200

    Mash topology on fake triangles for dataflow shader

--- unity/Holo/Assets/Scripts/model_with_plate/DataFlowVizualizer.cs
+++ unity/Holo/Assets/Scripts/model_with_plate/DataFlowVizualizer.cs
@@ -38,14 +38,11 @@ public class DataFlowVizualizer : MonoBehaviour
 
     private void CreateMesh()
     {
-        int linesCount = points[0].Count;
-        Vector3[] vertices = new Vector3[linesCount * 4];
+        int pointsCount = points[0].Count;
+        Vector3[] vertices = new Vector3[pointsCount];
+        Vector3[] vectorsAsNormals = new Vector3[pointsCount];
         for (int i = 0; i<vertices.Length; i++)
             vertices[i] = Vector3.zero;
-
-        //if base mesh should looks like first blendShape, now vertices in base mesh are 0,0,0:
-        //Array.Copy(points[0].ToArray(), vertices, points[0].Count);
-        //Array.Copy(endPoints[0].ToArray(), 0, vertices, points[0].Count, endPoints[0].Count);
 
         Mesh mesh = new Mesh();
         mesh.vertices = vertices; // baseVertices;
@@ -53,17 +50,20 @@ public class DataFlowVizualizer : MonoBehaviour
 
         for (int frame = 0; frame < frameCount; frame++)
         {
-            vertices = new Vector3[linesCount * 4];
-            Array.Copy(points[frame].ToArray(), vertices, linesCount);
-            Array.Copy(endPoints[frame].ToArray(), 0, vertices, linesCount * 1, linesCount);
-            Array.Copy(points[frame].ToArray(), 0, vertices, linesCount * 2, linesCount);
-            Array.Copy(endPoints[frame].ToArray(), 0, vertices, linesCount * 3, linesCount);
+            /*
+            vertices = new Vector3[pointsCount * 3];
+            Array.Copy(points[frame].ToArray(), vertices, pointsCount);
+            Array.Copy(endPoints[frame].ToArray(), 0, vertices, pointsCount * 1, pointsCount);
+            Array.Copy(points[frame].ToArray(), 0, vertices, pointsCount * 2, pointsCount);
             //put vector data inside normals data
-            Vector3[] vectorsAsNormals = new Vector3[linesCount * 4];
-            Array.Copy(vectors[frame].ToArray(), vectorsAsNormals, linesCount);
-            Array.Copy(vectors[frame].ToArray(), 0, vectorsAsNormals, linesCount * 1, linesCount);
-            Array.Copy(vectors[frame].ToArray(), 0, vectorsAsNormals, linesCount * 2, linesCount);
-            Array.Copy(vectors[frame].ToArray(), 0, vectorsAsNormals, linesCount * 3, linesCount);
+            Vector3[] vectorsAsNormals = new Vector3[pointsCount * 3];
+            Array.Copy(vectors[frame].ToArray(), vectorsAsNormals, pointsCount);
+            Array.Copy(vectors[frame].ToArray(), 0, vectorsAsNormals, pointsCount * 1, pointsCount);
+            Array.Copy(vectors[frame].ToArray(), 0, vectorsAsNormals, pointsCount * 2, pointsCount);
+            */
+
+            vertices = points[frame].ToArray();
+            vectorsAsNormals = vectors[frame].ToArray();
 
             //add blendShape
             mesh.AddBlendShapeFrame(frame.ToString(), 100f, vertices, vectorsAsNormals, null); //last null is a tangentNormal, we can put there alpha and beta data
@@ -74,19 +74,17 @@ public class DataFlowVizualizer : MonoBehaviour
             Debug.LogWarning("points[0].Count != vectors[0].Count " + points[0].Count + " " + endPoints[0].Count);
         }
 
-        Vector2[] uvs = new Vector2[vertices.Length];
-        int[] indices = new int[linesCount * 4];
+        Vector2[] uvs = new Vector2[pointsCount * 3];
+        int[] indices = new int[pointsCount * 3];
 
-        for (int i = 0; i < indices.Length / 4; i++)
+        for (int i = 0; i < indices.Length / 3; i++)
         {
-            indices[4 * i] =     (linesCount * 0) + i; //start point
-            indices[4 * i + 1] = (linesCount * 2) + i; //end point
-            indices[4 * i + 2] = (linesCount * 1) + i; //start point
-            indices[4 * i + 3] = (linesCount * 3) + i; //end point
-            uvs[0 * linesCount + i] = new Vector2(0, 0);
-            uvs[1 * linesCount + i] = new Vector2(1, 0);
-            uvs[2 * linesCount + i] = new Vector2(1, 1);
-            uvs[3 * linesCount + i] = new Vector2(0, 1);            
+            indices[3 * i] =     i; //start point
+            indices[3 * i + 1] = i; //end point
+            indices[3 * i + 2] = i; //start point
+            uvs[0 * pointsCount + i] = new Vector2(0, 0);
+            uvs[1 * pointsCount + i] = new Vector2(0, 0);
+            uvs[2 * pointsCount + i] = new Vector2(0, 0);          
         }
 
         mesh.uv = uvs;
@@ -98,13 +96,13 @@ public class DataFlowVizualizer : MonoBehaviour
             // - Lines: Each two indices in the mesh index buffer form a line.
             // - LineStrip:  First two indices form a line, and then each new index connects a new vertex to the existing line strip.
             // - Points: In most of use cases, mesh index buffer should be "identity": 0, 1, 2, 3, 4, 5, ...
-            MeshTopology.Quads,
+            MeshTopology.Triangles,
             // submesh
             0);
 
         // save Mesh to file
-        AssetDatabase.DeleteAsset("Assets/test_quads.mesh");
-        AssetDatabase.CreateAsset(mesh, "Assets/test_quads.mesh");
+        AssetDatabase.DeleteAsset("Assets/test_faketris.mesh");
+        AssetDatabase.CreateAsset(mesh, "Assets/test_faketris.mesh");
 
         GameObject go = new GameObject("Test Mesh SetTopology");
         MeshFilter meshFilter = go.AddComponent<MeshFilter>();
@@ -115,8 +113,8 @@ public class DataFlowVizualizer : MonoBehaviour
         go.AddComponent<BlendShapeAnimation>();
 
         // save GameObject to file
-        AssetDatabase.DeleteAsset("Assets/test_quads_game_object.prefab");
-        PrefabUtility.SaveAsPrefabAsset(go, "Assets/test_quads_game_object.prefab");
+        AssetDatabase.DeleteAsset("Assets/test_faketris_game_object.prefab");
+        PrefabUtility.SaveAsPrefabAsset(go, "Assets/test_faketris_game_object.prefab");
     }
 
     void ReadVTKData(int frame, string fileVtk)
