7ec1448e2db1215cdda9431cbeeaa954dc990ca4
Michalis Kamburelis <michalis.kambi@gmail.com>
2019-07-25 12:28:04 +0200

    Split the shared experience stuff into another scene, SharedExperienceAdditions.unity, to allow easier merging

---
+++ unity/Holo/Assets/Scripts/AddSharedExperienceScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AddSharedExperienceScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene("SharedExperienceAdditions", LoadSceneMode.Additive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
