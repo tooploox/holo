f7d38de71a8ffb0130fc52986308434b8c071410
Michalis Kamburelis <michalis.kambi@gmail.com>
2019-12-13 20:20:57 +0100

    Prepare model for preview: set blend shape, material

--- unity/Holo/Assets/Editor/ModelLoad/ImportedModel.cs
+++ unity/Holo/Assets/Editor/ModelLoad/ImportedModel.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 using ModelLoad.ModelImport;
@@ -17,6 +18,27 @@ namespace ModelLoad
             SaveFilesForExport(layerInfo, objectName, seriesImporter.ModelMesh.Get(), seriesImporter.ModelGameObject);
         }
 
+        private const string DefaultMaterialAsset = "Assets/GFX/Materials/DefaultModelMaterial.mat";
+
+        // Prepare the go for taking preview icon.
+        // We need to configure blend shapes state, material -- otherwise icon would look bad.
+        private void PrepareForPreview(GameObject go)
+        {
+            SkinnedMeshRenderer renderer = go.GetComponent<SkinnedMeshRenderer>();
+            if (renderer != null && 
+                renderer.sharedMesh != null &&
+                renderer.sharedMesh.blendShapeCount != 0)
+            {
+                renderer.SetBlendShapeWeight(0, 100f);
+                Material defaultMaterial = AssetDatabase.LoadAssetAtPath<Material>(DefaultMaterialAsset);
+                if (defaultMaterial == null)
+                {
+                    throw new Exception("Cannot read default material asset from " + DefaultMaterialAsset);
+                }
+                renderer.material = defaultMaterial;
+            }
+        }
+
         // Saves imported model to a Unity-friendly files, to be put in AssetBundles.
         private void SaveFilesForExport(ModelLayerInfo layerInfo, string objectName, Mesh modelMesh, GameObject modelGameObject)
         {
@@ -32,6 +54,7 @@ namespace ModelLoad
             PrefabUtility.SaveAsPrefabAsset(modelGameObject, gameObjectPath);
 
             if (layerInfo.UseAsIcon) {
+                PrepareForPreview(modelGameObject);
                 LayerAutomaticIconGenerate(modelGameObject);
             }
         }
