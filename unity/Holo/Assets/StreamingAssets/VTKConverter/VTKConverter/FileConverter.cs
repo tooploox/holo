141a02fd1bb50ba05a12e6ce3591cb00841ae61f
Bartlomiej <bartlomiej.kuchnowski@microscopeit.com>
2019-12-20 16:17:12 +0100

    HOL-43 Implemented Cell reading.

--- unity/Holo/Assets/StreamingAssets/VTKConverter/VTKConverter/FileConverter.cs
+++ unity/Holo/Assets/StreamingAssets/VTKConverter/VTKConverter/FileConverter.cs
@@ -7,31 +7,22 @@ namespace VTKConverter
     {
         public void Convert(string path, bool simulationFlag)
         {
-            vtkUnstructuredGrid vtkModel = ReadVTKData(path);
+            vtkDataSet vtkModel = ReadVTKData(path);
             ModelData modelData = new ModelData(vtkModel, simulationFlag);
             WriteModelToFile(modelData);
         }
 
-        private vtkUnstructuredGrid ReadVTKData(string path)
+        private vtkDataSet ReadVTKData(string path)
         {
             using (vtkDataSetReader reader = new vtkDataSetReader())
             {
                 //TODO: Can I use vtkDataSet and leave it at that or Do I need to use PolyData/UnstructuredGridReader
                 reader.SetFileName(path);
-                vtkPolyData readerOutput = reader.GetPolyDataOutput();
-                return Polydata2UnstructuredGrid(readerOutput);
+                reader.Update();
+                vtkDataSet readerOutput = reader.GetOutput();
+                return readerOutput;
             }
             
-        }
-
-        private vtkUnstructuredGrid Polydata2UnstructuredGrid(vtkPolyData input)
-        {
-            vtkAppendFilter appendFilter = new vtkAppendFilter();
-            appendFilter.SetInput(input);
-            appendFilter.Update();
-            vtkUnstructuredGrid unstructuredGrid = appendFilter.GetOutput();
-
-            return unstructuredGrid;
         }
 
         private void WriteModelToFile(ModelData modelData)
--- unity/Holo/Assets/StreamingAssets/VTKConverter/VTKConverter/ModelData.cs
+++ unity/Holo/Assets/StreamingAssets/VTKConverter/VTKConverter/ModelData.cs
@@ -1,30 +1,48 @@
-﻿using System;
-using System.Linq;
-using Kitware.VTK;
+﻿using Kitware.VTK;
 
 namespace VTKConverter
 {
     class ModelData
     {
-        public float[] BoundingBox { get; private set; }
-        public float[] Vertices {get; private set;}
+        public double[] BoundingBox { get; private set; }
+        public float[] Vertices { get; private set; }
         public int[] Indices { get; private set; }
         public float[] Vectors { get; private set; }
         public float[] Scalars { get; private set; }
 
-        public ModelData(vtkUnstructuredGrid vtkModel, bool simulationFlag)
+        public ModelData(vtkDataSet vtkModel, bool simulationFlag)
         {
             //TODO: Implement reading vertices cells if simulation - vectors and colours
+            BoundingBox = vtkModel.GetBounds();
+            SetIndices(vtkModel);
         }
 
-        private vtkPolyData Triangulate(vtkPolyData input)
+        private void SetVertices()
         {
-            vtkTriangleFilter triangleFilter = new vtkTriangleFilter();
-            triangleFilter.SetInput(input);
 
-            vtkPolyData output = triangleFilter.GetOutput();
+        }
 
-            return output;
+        private void SetIndices(vtkDataSet vtkModel)
+        {
+            int numberOfCells = vtkModel.GetNumberOfCells();
+            int cellSize = vtkModel.GetMaxCellSize();
+            Indices = new int[cellSize * numberOfCells];
+            int currentIndexNumber = 0;
+            for (int i = 0; i < numberOfCells; i++)
+            {
+                currentIndexNumber = GetCellIndices(currentIndexNumber, vtkModel.GetCell(i).GetPointIds());
+            }
+        }
+
+        private int GetCellIndices(int currentIndexNumber, vtkIdList cellIndices)
+        {
+            int numberOfIndices = cellIndices.GetNumberOfIds();
+            for (int j = 0; j < numberOfIndices; j++)
+            {
+                Indices[currentIndexNumber] = cellIndices.GetId(j);
+                currentIndexNumber += 1;
+            }
+            return currentIndexNumber;
         }
     }
 }
