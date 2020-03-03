c020e30612898f170e476b3f53c811da2cc87e91
Marek Zdonek <marek.zdonek@aeolusdev.com>
2020-03-03 08:46:29 +0100

    Fixes after review

--- unity/Holo/Assets/Scripts/models_collection/VolumetricLoader.cs
+++ unity/Holo/Assets/Scripts/models_collection/VolumetricLoader.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using UnityEditor;
+﻿using System.IO;
 using UnityEngine;
 
 public class VolumetricLoader : MonoBehaviour
