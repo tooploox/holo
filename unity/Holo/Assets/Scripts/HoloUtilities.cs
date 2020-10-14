067e2967ba397020727a3167b0d19cb58124bb01
Jeremi Kaczmarczyk <jeremi.kaczmarczyk@gmail.com>
2020-10-14 11:06:51 +0200

    Fixed HoloUtilities file to MRTK2

--- unity/Holo/Assets/Scripts/HoloUtilities.cs
+++ unity/Holo/Assets/Scripts/HoloUtilities.cs
@@ -1,25 +1,21 @@
-﻿using System;
-
-using UnityEngine;
-
-using HoloToolkit.Unity;
-using HoloToolkit.Unity.UX;
-using HoloToolkit.Unity.Buttons;
+﻿using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+using TMPro;
 
 public static class HoloUtilities
 {
     private static Color ButtonActiveColor = new Color(0f, 0.90f, 0.88f);
     private static Color ButtonInactiveColor = new Color(1f, 1f, 1f);
 
-    public static void SetButtonState(CompoundButton button, bool active)
+    public static void SetButtonState(PressableButtonHoloLens2 button, bool active)
     {
-        CompoundButtonIcon icon = button.GetComponent<CompoundButtonIcon>();
+        var icon = button.GetComponent<ButtonConfigHelper>();
         if (icon == null)
         {
             Debug.LogWarning("Missing CompoundButtonIcon on " + button.name);
             return;
         }
-        MeshRenderer iconRenderer = icon.IconMeshFilter.GetComponent<MeshRenderer>();
+        MeshRenderer iconRenderer = icon.GetComponent<MeshRenderer>();
         if (iconRenderer == null)
         {
             Debug.LogWarning("Missing MeshRenderer on CompoundButtonIcon.IconMeshFilter attached to " + button.name);
@@ -33,15 +29,15 @@ public static class HoloUtilities
         iconRenderer.material.SetColor("_Color", newColor);
     }
 
-    public static void SetButtonStateText(CompoundButton button, bool active)
+    public static void SetButtonStateText(PressableButtonHoloLens2 button, bool active)
     {
-        CompoundButtonText text = button.GetComponent<CompoundButtonText>();
+        var text = button.GetComponent<ButtonConfigHelper>();
         if (text == null)
         {
             Debug.LogWarning("Missing CompoundButtonText");
             return;
         }
-        TextMesh textMesh = text.TextMesh;
+        TextMeshPro textMesh = text.GetComponent<TextMeshPro>();
         if (textMesh == null)
         {
             Debug.LogWarning("Missing TextMesh");
--- unity/Holo/Assets/Scripts/buttons/IClickHandler.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public interface IClickHandler {
-    void Click(GameObject clickObject);
-    void FocusEnter(GameObject focusEnterObject);
-    void FocusExit(GameObject focusExitObject);
-}
