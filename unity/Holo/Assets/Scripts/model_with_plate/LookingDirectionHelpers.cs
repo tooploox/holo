0ae4a646caafddd9f406a1bcdd5f1278a2de6d1a
Marek Zdonek <marek.zdonek@aeolusdev.com>
2021-04-21 14:06:35 +0200

    Working configuration for spatial mapping and initial pose validaion

---
+++ unity/Holo/Assets/Scripts/model_with_plate/LookingDirectionHelpers.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.SpatialAwareness;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using UnityEngine;
+
+namespace MRTKExtensions.Utilities
+{
+    public static class LookingDirectionHelpers
+    {
+        public static Vector3 GetPositionInLookingDirection(float maxDistance = 2)
+        {
+            var hitPoint = GetPositionOnSpatialMap(maxDistance);
+
+            return hitPoint ?? CalculatePositionDeadAhead(maxDistance);
+        }
+
+        private static int _meshPhysicsLayer = 0;
+
+        private static int GetSpatialMeshMask()
+        {
+            if (_meshPhysicsLayer == 0)
+            {
+                var spatialMappingConfig = CoreServices.SpatialAwarenessSystem.ConfigurationProfile as
+                    MixedRealitySpatialAwarenessSystemProfile;
+                if (spatialMappingConfig != null)
+                {
+                    foreach (var config in spatialMappingConfig.ObserverConfigurations)
+                    {
+                        var observerProfile = config.ObserverProfile
+                            as MixedRealitySpatialAwarenessMeshObserverProfile;
+                        if (observerProfile != null)
+                        {
+                            _meshPhysicsLayer |= (1 << observerProfile.MeshPhysicsLayer);
+                        }
+                    }
+                }
+            }
+
+            return _meshPhysicsLayer;
+        }
+
+        public static Vector3? GetPositionOnSpatialMap(float maxDistance = 2)
+        {
+            RaycastHit hitInfo;
+            var transform = CameraCache.Main.transform;
+            var headRay = new Ray(transform.position, transform.forward);
+            if (Physics.Raycast(headRay, out hitInfo, maxDistance, GetSpatialMeshMask()))
+            {
+                return hitInfo.point;
+            }
+            return null;
+        }
+
+        public static Vector3 CalculatePositionDeadAhead(float distance = 2)
+        {
+            return CameraCache.Main.transform.position +
+                   CameraCache.Main.transform.forward.normalized * distance;
+        }
+    }
+}
---
+++ unity/Holo/Assets/Scripts/model_with_plate/SpatialPlacement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+using Microsoft.MixedReality.Toolkit.Extensions;
+
+using Microsoft.MixedReality.Toolkit.SpatialAwareness;
+using Microsoft.MixedReality.Toolkit;
+using MRTKExtensions.Utilities;
+
+public class SpatialPlacement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check raycast hit each time transformation is changed
+    }
+
+
+    public bool validatePosition()
+    {
+        float _maxDistance = 3;
+
+#if !UNITY_EDITOR
+        var foundPosition = LookingDirectionHelpers.GetPositionOnSpatialMap(_maxDistance);
+#else
+        Vector3? foundPosition = LookingDirectionHelpers.GetPositionInLookingDirection(_maxDistance);
+#endif
+        Debug.Log("Found position: " + foundPosition.ToString());
+        return foundPosition != null;
+    }
+
+
+    void OnEnable()
+    {
+        if (CoreServices.SpatialAwarenessSystem != null && !IsObserverRunning)
+        {
+            CoreServices.SpatialAwarenessSystem.ResumeObservers();
+            Debug.Log("Validate position result: " + validatePosition());
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (CoreServices.SpatialAwarenessSystem != null && IsObserverRunning)
+        {
+            CoreServices.SpatialAwarenessSystem.SuspendObservers();
+            CoreServices.SpatialAwarenessSystem.ClearObservations();
+        }
+    }
+
+    void OnTransformParentChanged()
+    {
+        if (!gameObject.activeSelf) return;
+    }
+
+    private bool IsObserverRunning
+    {
+        get
+        {
+            var providers =
+                ((IMixedRealityDataProviderAccess)CoreServices.SpatialAwarenessSystem)
+                .GetDataProviders<IMixedRealitySpatialAwarenessObserver>();
+            return providers.FirstOrDefault()?.IsRunning == true;
+        }
+    }
+}
