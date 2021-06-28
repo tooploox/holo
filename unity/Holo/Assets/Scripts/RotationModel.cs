239fb8849d1286fb7a9679f72eba26b4bad01ae1
Marek Zdonek <marek.zdonek@aeolusdev.com>
2021-06-28 16:27:38 +0200

    Rotation script for recording videos of assets

---
+++ unity/Holo/Assets/Scripts/RotationModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationModel : MonoBehaviour
+{
+    // 360 degrees / 5 sec => 2.4 degree / frame
+    public float RotationSpeed = 1.0f;
+    public GameObject rotationObject;
+
+    private bool rottationRunning = false;
+    private Quaternion originalRotation = new Quaternion();
+
+    private void OnEnable()
+    {
+        originalRotation = rotationObject.transform.rotation;
+        rottationRunning = true;
+    }
+
+    private void OnDisable()
+    {
+        rottationRunning = false;
+        rotationObject.transform.rotation = originalRotation;
+    }
+
+    private float GetRotationDelta()
+    {
+        float baseDelta = 0.4f;
+        return baseDelta * RotationSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rottationRunning)
+        {
+            rotationObject.transform.Rotate(0, GetRotationDelta(), 0);
+        }
+    }
+}
